4f92d0aaf4cb549faf0bd9187bd16171e55a3f9f
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2023-06-14 14:15:18 +0700

    first commit

---
+++ lib/pages/modulpage/modul_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:lat_state_management_flutter/pages/modulpage/done_module_list.dart';
+import 'package:lat_state_management_flutter/pages/modulpage/module_list.dart';
+
+class ModulPage extends StatefulWidget {
+  const ModulPage({Key? key}) : super(key: key);
+
+  @override
+  _ModulPageState createState() => _ModulPageState();
+}
+
+class _ModulPageState extends State<ModulPage> {
+  final List<String> doneModuleList = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Memulai Pemrograman Dengan Dart'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DoneModuleList(
+                    doneModuleList: doneModuleList,
+                  ),
+                ),
+              );
+            },
+            icon: const Icon(Icons.done),
+          )
+        ],
+      ),
+      body: ModuleList(
+        doneModuleList: doneModuleList,
+      ),
+    );
+  }
+}
